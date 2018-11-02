e26e338b245304155279fdfa20b6b8ea26825707
Zonghao Li <675375410@qq.com>
2018-11-02 00:34:33 +0000

    findBookByName accurate and fuzzy search and exception test

--- test/books-test.js
+++ test/books-test.js
@@ -47,5 +47,46 @@ describe('Books', function () {
                     });
             });
         });
+        //find a book by name and fuzzy search and exception
+        describe('GET /books/name=:name', () => {
+            it('should return a book by name', function(done) {
+                chai.request(server)
+                    .get('/books/name=them')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body.length).to.equal(1);
+                        let result = _.map(res.body, (book) => {
+                            return { name: book.name,
+                                author: book.author }
+                        });
+                        expect(result).to.include( { name: 'them', author: 'bgg' } );
+                        done();
+                    });
+            });
+            it('should return all books whose names contain letter \'e\'', function(done) {
+                chai.request(server)
+                    .get('/books/name=e')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body.length).to.equal(3);
+                        let result = _.map(res.body, (book) => {
+                            return { name: book.name,
+                                author: book.author }
+                        });
+                        expect(result).to.include( { name: 'them', author: 'bgg' } );
+                        expect(result).to.include( { name: 'her', author: 'mm' } );
+                        expect(result).to.include( { name: 'me', author: 'gf' } );
+                        done();
+                    });
+            });
+            it('should return a message that can not find the book by name', function(done) {
+                chai.request(server)
+                    .get('/books/name=123')
+                    .end(function(err, res) {
+                        expect(res.body).to.have.property('Message').equal( 'Sorry! Can\' find this book by name!');
+                        done();
+                    });
+            });
+        });
     });
 });
