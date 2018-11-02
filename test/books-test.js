a1efbe37b05d6141bc6dcd9944f8cb6a23a58e70
Zonghao Li <675375410@qq.com>
2018-11-02 00:33:37 +0000

    findBookByID and exception test

--- test/books-test.js
+++ test/books-test.js
@@ -21,5 +21,31 @@ describe('Books', function () {
                     });
             });
         });
+        //get a book by ID
+        describe('GET /books/id=:id', () => {
+            it('should return a book by id', function(done) {
+                chai.request(server)
+                    .get('/books/id=5bd0d75aa0fa610ec0cc092b')
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a('array');
+                        expect(res.body.length).to.equal(1);
+                        let result = _.map(res.body, (book) => {
+                            return { name: book.name,
+                                author: book.author }
+                        });
+                        expect(result).to.include( { name: 'me', author: 'gf' } );
+                        done();
+                    });
+            });
+            it('should return a message that can not find the book by id', function(done) {
+                chai.request(server)
+                    .get('/books/id=dsad')
+                    .end(function(err, res) {
+                        expect(res.body).to.have.property('Message').equal('Sorry! Can\' find this book by ID!' );
+                        done();
+                    });
+            });
+        });
     });
 });
