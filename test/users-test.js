4ff017fd8594ce821f2dd674fbdf10667fa4386d
Zonghao Li <675375410@qq.com>
2018-11-02 20:15:44 +0000

    test addUser

---
+++ test/users-test.js
@@ -0,0 +1,59 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../bin/www');
+let expect = chai.expect;
+
+chai.use(require('chai-things'));
+chai.use(chaiHttp);
+let _ = require('lodash' );
+describe('Users', function (){
+    describe('POSTs', function (){
+        describe('POST /users/addUser', function () {
+            it('should return confirmation message that add successfully and update database ', function(done) {
+                let user = {
+                    account: 'albert',
+                    psw: '123456',
+                    email: 'aaa@qq.com',
+                };
+                chai.request(server)
+                    .post('/users/addUser')
+                    .send(user)
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('message').equal('User created successfully!' );
+                        done();
+                    });
+            });
+            after(function (done) {
+                chai.request(server)
+                    .get('/users/acc=albert')
+                    .end(function(err, res) {
+                        let result = _.map(res.body, (user) => {
+                            return {
+                                account: user.account,
+                                psw: user.psw,
+                                email: user.email,
+                            };
+                        });
+                        expect(result).to.include( { account: 'albert', psw: '123456',email:'aaa@qq.com'  } );
+                        done();
+                    });
+            });
+            it('should return a failed message for creating user failed', function(done) {
+                let user = {
+                    account: 'albert',
+                    psw: '123456',
+                    email: 'aaa@qq.com',
+                };
+                chai.request(server)
+                    .post('/users/addUser')
+                    .send(user)
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('message').equal('User created failed...' );
+                        done();
+                    });
+            });
+        });
+    });
+});
