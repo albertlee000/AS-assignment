837e79b7620617acdc2dded4b72c2768de4a8cb6
Zonghao Li <675375410@qq.com>
2018-11-02 19:32:52 +0000

    tested increaseLike in normal and exception cases

---
+++ test/users-test.js
@@ -0,0 +1,46 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../bin/www');
+let expect = chai.expect;
+
+chai.use(require('chai-things'));
+chai.use(chaiHttp);
+let _ = require('lodash' );
+describe('Users', function (){
+    describe('PUTs', function (){
+        describe('PUT /users/like=:id', function () {
+            it('should return a message that user liked the book and the book\'s like number increased ', function(done) {
+                let bookname = {bookname:'you'};
+                chai.request(server)
+                    .put('/users/like=5bd0d4f056a059283002a29c')
+                    .send(bookname)
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('message').equal('You liked this book' );
+                        done();
+                    });
+            });
+            after(function  (done) {
+                chai.request(server)
+                    .get('/books/id=5bd0d747a0fa610ec0cc092a')
+                    .end(function(err, res) {
+                        let result = _.map(res.body, (book) => {
+                            return { like:book.like };
+                        }  );
+                        expect(result).to.include( { like:5781 } );
+                        done();
+                    });
+            });
+            it('should return a message that liked failed for this user has liked ', function(done) {
+                let bookname = {bookname:'you'};
+                chai.request(server)
+                    .put('/users/like=5bd0d4f056a059283002a29c')
+                    .send(bookname)
+                    .end(function(err, res) {
+                        expect(res.body).to.have.property('message').equal('You have liked this book, cannot like again...' );
+                        done();
+                    });
+            });
+        });
+    });
+});
