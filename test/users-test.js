210a6ee390ed7a3fa87729bcbac15ba8884bfffd
Zonghao Li <675375410@qq.com>
2018-11-02 19:48:47 +0000

    tested unlike in normal and exception cases and ensured it worked

--- test/users-test.js
+++ test/users-test.js
@@ -42,5 +42,39 @@ describe('Users', function (){
                     });
             });
         });
+        describe('PUT /users/unlike=:id', function () {
+            it('should return a message that user unlikes the book and the book\'s like decreased ', function(done) {
+                let bookname = {bookname:'you'};
+                chai.request(server)
+                    .put('/users/unlike=5bd0d4f056a059283002a29c')
+                    .send(bookname)
+                    .end(function(err, res) {
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.have.property('message').equal('You unliked this book');
+                        done();
+                    });
+            });
+            after(function  (done) {
+                chai.request(server)
+                    .get('/books/id=5bd0d747a0fa610ec0cc092a')
+                    .end(function(err, res) {
+                        let result = _.map(res.body, (book) => {
+                            return { like:book.like };
+                        }  );
+                        expect(result).to.include( { like:5780 } );
+                        done();
+                    });
+            });
+            it('should return a message that unliked failed for this user has not liked the book', function(done) {
+                let bookname = {bookname:'you'};
+                chai.request(server)
+                    .put('/users/unlike=5bd0d4f056a059283002a29c')
+                    .send(bookname)
+                    .end(function(err, res) {
+                        expect(res.body).to.have.property('message').equal('You have not liked this book!' );
+                        done();
+                    });
+            });
+        });
     });
 });
